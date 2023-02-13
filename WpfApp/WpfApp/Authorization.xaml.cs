f9aed33b8d6f1dba4add3c3699d9f0ea2e7d9ec6
negroid1 <103184914+negroid1@users.noreply.github.com>
2023-02-13 15:28:26 +0500

    Add files via upload

---
+++ WpfApp/WpfApp/Authorization.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Page
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Login.Text = "";
+            Password.Text = "";
+        }
+
+        private void BtnInput_Click(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text == Login.Text)
+            {
+                if (Password.Text == Password.Text)
+                {
+                    if (Code.Text == Code.Text)
+                    {
+                        return;
+                    }
+                    else 
+                    {
+                        MessageBox.Show("Вы ввели неправильный код");
+                    }
+                }
+                else 
+                {
+                    MessageBox.Show("Вы ввели неправльный пароль");
+                }
+            }
+            else 
+            {
+                MessageBox.Show("Вы ввели неправльный логин");
+            }
+        }
+    }
+}
